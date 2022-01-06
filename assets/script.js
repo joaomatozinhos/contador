987ff07a1be7ccfc364f120077d59efd49b70c74
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2022-01-05 22:35:48 -0300

    start logic

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
+const data = document.getElementById('input-data')
+const btnIniciar = document.getElementById('btn-iniciar')
 
+btnIniciar.addEventListener('click', iniciarContagem)
+
+function iniciarContagem() {
+  let dataValor = data.value
+  let dataArray = dataValor.split('-')
+  let dia = Number(dataArray[2])
+  let mes = Number(dataArray[1]) - 1
+  let ano = Number(dataArray[0])
+
+  let dataAtual = new Date()
+
+  let dataFinal = new Date(ano, mes, dia)
+  console.log(dataFinal)
+  console.log(dataAtual)
+  let resultado = dataFinal - dataAtual
+  console.log(resultado)
+
+  const dias = Math.floor(result / 1000 / 60 / 60 / 24)
+
+  window.location.href = 'contador.html'
+}
